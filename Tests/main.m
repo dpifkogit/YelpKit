8709f647d8a3577f30d8008077681f6e06c684ba
Allen Cheung <acheung@yelp.com>
2013-06-18 17:38:26 -0700

    Adding Tests target

---
+++ Tests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tests
+//
+//  Created by Allen Cheung on 6/18/13.
+//  Copyright (c) 2013 Yelp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([YKAppDelegate class]));
+  }
+}
